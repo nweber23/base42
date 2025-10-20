198d28a8a06c1bc1f056d3a96c770b1cd4205191
Niklas Weber <nweber@student.42Heilbronn.de>
2025-10-20 18:19:03 +0200

    feat: add models for Event, Message, Project, and User

---
+++ base42-backend/src/models/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id: number;
+  login: string;
+  name: string;
+  level: number;
+  campus: string;
+  location: string;
+  favorites: string[];
+}
---
+++ base42-backend/src/models/index.ts
@@ -0,0 +1,4 @@
+export { User } from './User';
+export { Project } from './Project';
+export { Event } from './Event';
+export { Message } from './Message';
